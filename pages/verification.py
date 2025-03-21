8bf647608a850836b864ea5cf7a51ca3d7791a0e
mewaha3 <130312801+mewaha3@users.noreply.github.com>
2025-03-21 15:16:15 +0700

    Update verification.py

--- pages/verification.py
+++ pages/verification.py
@@ -1,4 +1,4 @@
-import streamlit as st
+Himport streamlit as st
 
 # ✅ ตั้งค่าหน้า
 st.set_page_config(page_title="Verification Result", page_icon="✅", layout="centered")
@@ -10,4 +10,4 @@ st.markdown("<h2 style='color:green; text-align:center;'>Pass Verification</h2>"
 
 # ✅ ปุ่มไปหน้า Profile
 st.page_link("pages/profile.py", label="Go to Profile", icon="👤")
-st.page_link("pages/home.py", label="Go to Profile", icon="🏠")
+st.page_link("pages/home.py", label="Go to Home", icon="🏠")
