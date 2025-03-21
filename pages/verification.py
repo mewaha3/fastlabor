6b32d8c6b84a2cacf4fffdbf1ef0de4cf07458d0
mewaha3 <130312801+mewaha3@users.noreply.github.com>
2025-03-21 15:11:16 +0700

    Create verification.py

---
+++ pages/verification.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# ✅ ตั้งค่าหน้า
+st.set_page_config(page_title="Verification Result", page_icon="✅", layout="centered")
+
+st.image("image.png", width=150)
+st.title("Result verification")
+
+st.markdown("<h2 style='color:green; text-align:center;'>Pass Verification</h2>", unsafe_allow_html=True)
+
+# ✅ ปุ่มไปหน้า Profile
+st.page_link("pages/profile.py", label="Go to Profile", icon="👤")
